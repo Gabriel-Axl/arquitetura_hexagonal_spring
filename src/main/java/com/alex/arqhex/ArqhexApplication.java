76e81185a8c2657e9f8feedc94d77cbf2fad02c8
alexgab <alexgabriel3357@gmail.com>
2024-09-23 14:10:08 -0300

    initial

---
+++ src/main/java/com/alex/arqhex/ArqhexApplication.java
@@ -0,0 +1,13 @@
+package com.alex.arqhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqhexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqhexApplication.class, args);
+	}
+
+}
